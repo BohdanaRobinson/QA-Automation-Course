b6b4d26de0a64551aa53fad82c502a8c026fb5c6
Bohdana Robinson <bogdana.goyuk@gmail.com>
2026-01-20 11:47:26 -0500

    Adding files Tesst

---
+++ Week-2/operators.js
@@ -0,0 +1,37 @@
+//1. Addition
+
+const number = 5;
+
+console.log(number + 3);
+
+// 2. Substraction
+console.log(9-4);
+
+//3. Multiplication
+console.log(2*4);
+
+
+
+//6. Modulus 
+
+
+// Comparison operators - Will give us a boolean data type//
+// "!" sign - foe boolean data types , reverse the value
+
+console.log(!(8>3));
+
+//1. Strict equal
+
+console.log (5===5);
+console.log(5===8);// 5 strictly equal.... 
+
+//2. Not strict equal
+console.log (5!==5);
+console.log(5!==8);
+
+
+//3. Greater than
+console.log(30  >20);
+
+//4. Less than 
+console.log(30 < 12);
