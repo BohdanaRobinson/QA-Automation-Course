98628ce229c32490e81dca17eee7a56fbe588c5d
Bohdana Robinson <bogdana.goyuk@gmail.com>
2026-01-20 23:08:10 -0500

    Added changes

---
+++ Week-2/Truthy-vs-Falsy-values.js
@@ -0,0 +1,9 @@
+if ("Hello") {
+  console.log("This is truthy value");
+}
+
+if (0) {
+  console.log("this is a falsy value");
+}
+let number = 6;
+console.log(number);
--- Week-2/operators.js
+++ Week-2/operators.js
@@ -1,37 +1,34 @@
 //1. Addition
 
 const number = 5;
-
 console.log(number + 3);
 
+console.log(5 + 3);
+
 // 2. Substraction
-console.log(9-4);
+console.log(9 - 4);
 
 //3. Multiplication
-console.log(2*4);
+console.log(2 * 4);
 
-
-
-//6. Modulus 
-
+//6. Modulus
 
 // Comparison operators - Will give us a boolean data type//
 // "!" sign - foe boolean data types , reverse the value
 
-console.log(!(8>3));
+console.log(!(8 > 3));
 
 //1. Strict equal
 
-console.log (5===5);
-console.log(5===8);// 5 strictly equal.... 
+console.log(5 === 5);
+console.log(5 === 8); // 5 strictly equal....
 
 //2. Not strict equal
-console.log (5!==5);
-console.log(5!==8);
-
+console.log(5 !== 5);
+console.log(5 !== 8);
 
 //3. Greater than
-console.log(30  >20);
+console.log(30 > 20);
 
-//4. Less than 
+//4. Less than
 console.log(30 < 12);
