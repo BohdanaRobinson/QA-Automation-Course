92b41c261819995cfc42b29dc722d7f8fcefea10
Bohdana Robinson <bogdana.goyuk@gmail.com>
2026-01-20 23:42:38 -0500

    Completed part1

--- Week-2/assignement3.js
+++ Week-2/assignement3.js
@@ -1,6 +1,20 @@
+// 1. Create two numbers and:
 let number = 20;
 console.log(number + 5);
 
 console.log(20 - 10);
 console.log(5 * 5);
 console.log(20 / 2);
+
+// 1.2 Create a number and
+let stockPrice = 200;
+stockPrice++;
+console.log(stockPrice);
+stockPrice--;
+console.log(stockPrice);
+
+//1.3 Create two numbers and print:(remainder)
+20 % 5;
+
+//1.4. create a number and raise it to thr power of 3
+console.log(3 ** 3);
