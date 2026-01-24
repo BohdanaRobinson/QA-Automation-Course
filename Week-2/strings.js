fec8596dbdec475b614ed16ae3ea126788995d2f
Bohdana Robinson <bogdana.goyuk@gmail.com>
2026-01-24 00:25:45 -0500

    Added new file of strings

---
+++ Week-2/strings.js
@@ -0,0 +1,23 @@
+///// Strings are the text data: " Hello", "Goodbye"
+
+//1.  .lenght - string lenght - tels you how many characters is your string ( including spaces)
+
+let myBoy = "Jaycee";
+console.log(myBoy.length); // 6
+
+//2.  Accesing Characters by their index(Starting from 0)
+
+const myDogName = "Jaycee";
+console.log(myDogName[3]);
+console.log(myDogName[0]);
+
+//3. .toUpperCase() & .toLowerCase() -- Changing  the letter case of a string
+
+let myDog = "Jaycee is a good boy";
+console.log(myDog.toUpperCase());
+console.log(myDog.toLowerCase());
+
+//4.  .trim() -- removes extra spaces fron start and end of the string
+
+let soccerClub = "    Barcelona    ";
+console.log(soccerClub.trim());
