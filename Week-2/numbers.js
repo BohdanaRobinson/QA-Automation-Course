8e8db215d7369e36ef941dee28256723c14ece91
Bohdana Robinson <bogdana.goyuk@gmail.com>
2026-01-24 12:50:16 -0500

    new file added

---
+++ Week-2/numbers.js
@@ -0,0 +1,23 @@
+/////------Numbers---------//////////
+// number represents a numeric values, icludind
+// whole numbers (integers)
+/// and decimal numbers(floating-point);
+
+let age = 35; // whole numbers
+let price = 6.99; // decimal number
+
+////-----Basic Number Operations////////
+//1. Addition
+console.log(5 + 3);
+
+// 2. Substraction
+console.log(9 - 4);
+
+//3. Multiplication
+console.log(2 * 4);
+
+//4. Division
+console.log(8 / 4);
+
+//5. Remainder
+console.log(8 % 3);
