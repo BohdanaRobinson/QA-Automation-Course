4c51f5546fd7846ff0281b93d4e9d86e32a825d8
Bohdana Robinson <bogdana.goyuk@gmail.com>
2026-01-24 01:10:31 -0500

    new cool changes added

--- Week-2/strings.js
+++ Week-2/strings.js
@@ -21,3 +21,38 @@ console.log(myDog.toLowerCase());
 
 let soccerClub = "    Barcelona    ";
 console.log(soccerClub.trim());
+
+//5.  .slice - slices start and end ofa string between two indexes
+
+let city = "NewYork";
+console.log(city.slice(2));
+console.log(city.slice(0, 3));
+
+// 6.  .replace() - search value, new value - replaces the first match of searchValue with newValue.
+
+let hobby = " I like watching soccer";
+console.log(hobby.replace("soccer", "UFC"));
+
+//7  .includes()  - checks if a string contains a specific susbstring.
+//return boolean value: true or false
+
+let statement = " I like traveling";
+console.log(statement.includes("traveling")); // true
+console.log(statement.includes("dancing")); // false
+
+//8.  .split - splits a string into an array using the separator you provide
+
+let ufcChampions = "Porier, Oliviera, Pereira, Jones";
+console.log(ufcChampions.split(","));
+//console.log(ufcChampions.split(""));
+
+//9.  .concat() - joins two or more strings together
+
+let s = "Software";
+let d = "Development";
+let l = "Lyfecycle";
+
+console.log(s.concat(" ", d, " ", l));
+
+// or //
+console.log(s + " " + d + " " + l);
