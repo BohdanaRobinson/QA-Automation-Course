2da2819bf66c0aa1b4782fb7a065c21995d99f9d
Bohdana Robinson <bogdana.goyuk@gmail.com>
2026-01-26 20:28:33 -0500

    New file topic added

---
+++ Week-3/functions.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+console.log("Hello World");
+
+function greetName() {
+  console.log("Hello world!");
+}
+greetName();
+
+function greet() {
+  const addition = 3 + 5;
+  console.log(addition);
+}
+greet();
