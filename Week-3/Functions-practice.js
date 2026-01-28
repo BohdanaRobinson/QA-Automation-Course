b67536a916907a22dbfc49adb7f3a5d55cb96161
Bohdana Robinson <bogdana.goyuk@gmail.com>
2026-01-27 23:54:17 -0500

    new homework

---
+++ Week-3/Functions-practice.js
@@ -0,0 +1,11 @@
+let carBrands = ["Bmw", "Nissan", "Mercedez", "Toyota"];
+console.log(carBrands);
+
+let numbers = [4, 55, 85, 25];
+console.log(numbers);
+
+let sentence = " It's so cool and convenient to own a car";
+console.log(sentence);
+
+let price = 9.99;
+console.log(price);
