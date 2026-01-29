587383c90c60717bf1e70bad96f72314e41afea3
Bohdana Robinson <bogdana.goyuk@gmail.com>
2026-01-28 23:05:23 -0500

    Ad

---
+++ Week-3/h.js
@@ -0,0 +1,22 @@
+let carBrands = ["Bmw", "Nissan", "Mercedez", "Toyota"];
+let numbers = [4, 55, 85, 25];
+let sentence = "It's so cool and convenient to own a car";
+let price = 9.99;
+
+// ## PART 1: STRING + FUNCTIONS
+
+// ### 🔤 Task 1: Clean & Format Text
+// Write a function that:
+
+// 1. Takes a string
+// 2. Removes extra spaces at the start and end
+// 3. Converts the string to lowercase
+// 4. Returns the cleaned string
+
+function firstTasks(str) {
+  let noSpaces = str.trim();
+  let lowerCase = noSpaces.toLowerCase();
+  return lowerCase;
+}
+
+console.log(firstTasks("  It's so cool and convenient to own a car  "));
