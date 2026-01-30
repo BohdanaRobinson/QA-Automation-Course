bd604b5f2fd0e597f5f0f79f2606f1fc60f5e544
Bohdana Robinson <bogdana.goyuk@gmail.com>
2026-01-29 23:34:21 -0500

    N

--- Week-3/objects.js
+++ Week-3/objects.js
@@ -3,6 +3,12 @@ const person = {
   name: "Alice",
   age: 25,
   isStudent: true,
+  greet: function () {
+    console.log("Hello my name is Dana");
+  },
+  bye() {
+    console.log("I am Dana, bye"); // this way is better to use
+  },
 };
 
 for (const key in person) {
@@ -29,3 +35,26 @@ person.height = 170; //Adding is height doesn't exist.
 console.log(person);
 
 // but it does exist , u write  the same but  it will update
+
+//5.   "delete" keyword - Removing properties
+delete person.height;
+console.log(person);
+
+//6. Object methods
+
+person.greet();
+person.bye();
+
+// 7.  Using "This" keyword
+
+const car = {
+  brand: "Tesla",
+  speed: 120,
+  info() {
+    console.log(`${this.brand} is going at ${this.speed} km/h`);
+  },
+};
+car.info();
+
+// 8. Built object methods
+// Object.keys , Object.values , Object.entries
