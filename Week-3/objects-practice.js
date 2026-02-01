23a54186d0df2ee620a2722db9c45d0fe3cb0c59
Bohdana Robinson <bogdana.goyuk@gmail.com>
2026-02-01 13:09:53 -0500

    New

---
+++ Week-3/objects-practice.js
@@ -0,0 +1,19 @@
+// ### Task 1: Object Creation
+// Create **one object** that represents something of your choice. (Examples: car, student, phone, video game, book, movie, etc.)
+// Your object must include:
+// - At least **4 properties**
+// - At least **1 string**
+// - At least **1 number**
+// - At least **1 nested object**
+
+const ufcFighter = {
+  name: "Charles Oliviera",
+  age: 38,
+  isUfcFighter: true,
+  country3: {
+    countryName: "Brazil",
+    city: "Sao Paulo",
+    zipCode: 565656,
+  },
+};
+console.log(ufcFighter);
