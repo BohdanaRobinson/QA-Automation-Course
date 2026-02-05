72d74a5b21026b0dde81ff18b7c3b635a0b461b6
Bohdana Robinson <bogdana.goyuk@gmail.com>
2026-02-05 00:39:28 -0500

    NEW

--- Week-4/advanced-arrays.js
+++ Week-4/advanced-arrays.js
@@ -6,8 +6,20 @@ const numbers = [3, 6, 9, 12, 15];
 const tripled = numbers.map(function (num) {
   return num * 3;
 });
+console.log(tripled);
+
+//or with arrow function
+// const tripled = numbers.map(num => num * 3);
 
 // filter() --> keeps only the elements that passes a test and return a new array
 
 const numbersGreatestTen = numbers.filter((num) => num > 10);
 console.log(numbersGreatestTen);
+
+//or it does the same with loop:
+
+const trippled = [];
+
+for (let i = 0; i < numbers.length; i++) {
+  trippled.push(numbers[i] * 3);
+}
