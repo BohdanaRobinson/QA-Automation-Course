9996a9837cde2e52305537578a71ab5803430942
Bohdana Robinson <bogdana.goyuk@gmail.com>
2026-02-11 17:43:15 -0500

    I

--- Week-5/inheritance.js
+++ Week-5/inheritance.js
@@ -16,8 +16,48 @@ class SlowCar extends Car {
 }
 
 const slooowCar = new SlowCar();
-slooowCar.slowcarveryslowdring();
+slooowCar.slowcarveryslowdring(); //i'm slow
 
 const ferrari = new SportsCar();
 ferrari.drive(); // Driving
 ferrari.turboBoost(); /// Turbo boost activated
+
+///////Inheritance with extends and super
+
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sleep() {
+    console.log(`${this.name} is sleeping}`);
+  }
+
+  eat() {
+    console.log(`${this.name} is eating now`);
+  }
+
+  introduce() {
+    console.log(
+      `the animal's name is ${this.name} and it's ${this.age} years old`,
+    );
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, breed) {
+    super(name, age); // Call parent class constructor
+    this.breed = breed;
+  }
+
+  bark() {
+    console.log("woof");
+  }
+}
+
+const dog = new Dog("Buddy", 3, "boxer");
+dog.sleep(); //Buddy is sleeping
+dog.eat();
+dog.introduce();
+dog.bark();
