8ef9c3185149ebf9cbe1427376f3f44ea32d698b
Bohdana Robinson <bogdana.goyuk@gmail.com>
2026-02-16 22:09:32 -0500

    T

--- Week-5/async-practice.js
+++ Week-5/async-practice.js
@@ -51,16 +51,63 @@ prepareGreeting(function (message) {
 function makePizza() {
   return new Promise((resolve, reject) => {
     let pizzaAvailable = true;
-    if (pizzaAvailable) resolve("Pizza is ready");
-    else {
-      reject("No igradients");
-    }
-  }, 2000);
+
+    setTimeout(() => {
+      if (pizzaAvailable) resolve("Pizza is ready");
+      else {
+        reject("No igradients");
+      }
+    }, 2000);
+  });
 }
+
 makePizza()
   .then((message) => {
     console.log(message);
   })
   .catch((error) => {
     console.log(error);
+  });
+
+//   ## Task 3: Promise with Number Check
+
+// Create a function:
+
+// ```jsx
+// checkNumber(num)
+// ```
+
+// It should:
+
+// 1. Return a Promise
+// 2. After 1 second:
+//     - Resolve with `"Even number"` if the number is even
+//     - Reject with `"Odd number"` if the number is odd
+
+// Call the function and handle results using:
+
+// ```jsx
+// .then()
+// .catch()
+// ```
+
+function checkNumber(num) {
+  return new Promise((resolve, rejected) => {
+    setTimeout(() => {
+      if (num % 2 === 0) {
+        /// check is number is even
+        resolve("Even number");
+      } else {
+        reject("Odd number");
+      }
+    }, 1000);
+  });
+}
+
+checkNumber(10)
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((message) => {
+    console.log(message);
   });
