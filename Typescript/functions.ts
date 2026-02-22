c19349d8e35a3a0244b8ba8c50959015084f446a
Bohdana Robinson <bogdana.goyuk@gmail.com>
2026-02-22 16:11:40 -0500

    N

--- Typescript/functions.ts
+++ Typescript/functions.ts
@@ -3,6 +3,41 @@ function greet(name: string, age: number) {
 }
 greet("Dana", 25);
 
-// function checkBalance(amount: number) : Promise<number> {
-const balance = 500;
-//}//
+function greetUser(name: string = "Guest") {
+  console.log(`hello I am ${name}`);
+}
+greetUser();
+greetUser("Dana");
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+add(2, 3);
+
+function printSmth(): void {
+  console.log(`Hello`);
+}
+printSmth();
+
+function checkBalance(amount: number): Promise<string> {
+  const balance = 500;
+
+  if (amount <= balance) {
+    return Promise.resolve(
+      `Withdrawal of ${amount} succesful. Remaining balance: ${balance - amount}`,
+    );
+  } else {
+    return Promise.reject("Insufficient funds");
+  }
+}
+
+async function performWithdarawal() {
+  try {
+    const result = await checkBalance(200);
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+performWithdarawal();
