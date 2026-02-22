b281751a20e155e9633f186bbddf78f69d523e4f
Bohdana Robinson <bogdana.goyuk@gmail.com>
2026-02-22 15:05:10 -0500

    Functions

---
+++ Typescript/functions.ts
@@ -0,0 +1,8 @@
+function greet(name: string, age: number) {
+  console.log(`Hello, my name is ${name} and my age ${age}`);
+}
+greet("Dana", 25);
+
+// function checkBalance(amount: number) : Promise<number> {
+const balance = 500;
+//}//
